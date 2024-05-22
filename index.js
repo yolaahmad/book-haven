9f779f5707a74f82411e6c586c8b9d8070e233c1
Rilwan Jibril <rilwangabi10@gmail.com>
2024-05-22 15:05:41 +0100

    commit test

--- index.js
+++ index.js
@@ -19,46 +19,19 @@
 //     pEl.textContent = 'LOGIN'
 // })
 
-const logLink = document.getElementById("login-link");
-const containerSignUp = document.querySelector(".sign-up");
-const containerLogin = document.querySelector(".login");
-const signLink = document.getElementById("sign-link");
+const logLink = document.getElementById('login-link');
+const containerSignUp = document.querySelector('.sign-up');
+const containerLogin = document.querySelector('.login');
+const signLink = document.getElementById('sign-link');
 
-logLink.addEventListener("click", () => {
-  console.log("clicked");
-  containerSignUp.classList.toggle("rotate-sign");
-  containerLogin.classList.toggle("rotate-log");
+logLink.addEventListener('click', () => {
+    console.log('clicked');
+    containerSignUp.classList.toggle('rotate-sign');
+    containerLogin.classList.toggle('rotate-log'); 
 });
 
-signLink.addEventListener("click", () => {
-  containerLogin.classList.toggle("rotate-log");
-  containerSignUp.classList.remove("rotate-sign");
+signLink.addEventListener('click', () => {
+    containerLogin.classList.toggle('rotate-log');
+    containerSignUp.classList.remove('rotate-sign');
 });
 
-function togglePass() {
-  var x = document.getElementById("PassInput");
-  if (x.type === "password") {
-    document.getElementById("eyeIcon").style.display = "none";
-    document.getElementById("hidePass").style.display = "block";
-    x.type = "text";
-  } else {
-    document.getElementById("eyeIcon").style.display = "block";
-    document.getElementById("hidePass").style.display = "none";
-    x.type = "password";
-  }
-}
-
-//Toggle for login eye-icon
-
-function togglePass2() {
-  var x = document.getElementById("PassInput2");
-  if (x.type === "password") {
-    document.getElementById("eyeIcon2").style.display = "none";
-    document.getElementById("hidePass2").style.display = "block";
-    x.type = "text";
-  } else {
-    document.getElementById("eyeIcon2").style.display = "block";
-    document.getElementById("hidePass2").style.display = "none";
-    x.type = "password";
-  }
-}
